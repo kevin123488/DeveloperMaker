a23c2758f663900800a784cfbc25d512c01d03f6
정찬우 <jeong_2240@naver.com>
2022-10-17 10:40:02 +0900

    Create: dev/front 및 기본 구조 작성

---
+++ front/src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { store } from './slices/index.js';
+import Main from './pages/Main';
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+
+root.render(
+    <Provider store={store}>
+      <Main />
+    </Provider>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
