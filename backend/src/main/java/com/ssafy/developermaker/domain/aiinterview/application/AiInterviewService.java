5ab81a1b9d07d09556e7ae3e3a5466cafbaa783e
남한솔 <hansol620915@naver.com>
2022-10-25 17:51:13 +0900

    [BE] Add: AiInterview
    - AI 인터뷰 Entity 생성 및 Service 작성

--- backend/src/main/java/com/ssafy/developermaker/domain/aiinterview/application/AiInterviewService.java
+++ backend/src/main/java/com/ssafy/developermaker/domain/aiinterview/application/AiInterviewService.java
@@ -5,12 +5,14 @@ import kong.unirest.JsonNode;
 import kong.unirest.Unirest;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-
+import kr.co.shineware.nlp.komoran.constant.DEFAULT_MODEL;
+import kr.co.shineware.nlp.komoran.core.Komoran;
+import kr.co.shineware.nlp.komoran.model.KomoranResult;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
-public class EmotionService {
+public class AiInterviewService {
     @Value("${properties.file.luxand-token}") private String TOKEN;
     private HttpResponse<JsonNode> post_request(String url, Map<String, Object> fields) {
         return Unirest.post(url)
@@ -18,15 +20,21 @@ public class EmotionService {
                 .fields(fields)
                 .asJson();
     }
-    public String getResult(String url){
+    public String getResult(String url, String aiInterviewText){
+        String result = null;
         HttpResponse<JsonNode> response = post_request("https://api.luxand.cloud/photo/emotions",
                 new HashMap<String, Object>() {{ put("photo", url); }});
 
         if (response.getStatus() != 200){
             System.out.printf(response.getBody().getObject().getString("message"));
-            System.exit(1);
         }
+//(response.getBody().toString())
 
-        return (response.getBody().toString());
+        Komoran komoran = new Komoran(DEFAULT_MODEL.FULL);
+        KomoranResult analyzeResultList = komoran.analyze(aiInterviewText);
+
+
+
+        return result;
     }
 }
--- backend/src/main/java/com/ssafy/developermaker/domain/aiinterview/controller/AiInterviewController.java
+++ backend/src/main/java/com/ssafy/developermaker/domain/aiinterview/controller/AiInterviewController.java
@@ -1,6 +1,7 @@
 package com.ssafy.developermaker.domain.aiinterview.controller;
 
-import com.ssafy.developermaker.domain.aiinterview.application.EmotionService;
+import com.ssafy.developermaker.domain.aiinterview.application.AiInterviewService;
+import com.ssafy.developermaker.domain.aiinterview.dto.AiInterviewRequestDto;
 import com.ssafy.developermaker.global.model.BaseResponseBody;
 import com.ssafy.developermaker.global.util.AwsS3Service;
 import io.swagger.annotations.ApiParam;
@@ -9,16 +10,19 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-@CrossOrigin(origins = { "*" }, maxAge = 6000)
+@CrossOrigin(origins = {"*"}, maxAge = 6000)
 @RestController
 @RequestMapping("/ai")
 @RequiredArgsConstructor
 public class AiInterviewController {
-    private final EmotionService emotionService;
+    private final AiInterviewService aiInterviewService;
     private final AwsS3Service awsS3Service;
+
     @PostMapping
-    public ResponseEntity<BaseResponseBody> getResult(@RequestPart (value = "file") @ApiParam(value="이미지 파일") MultipartFile file){
+    public ResponseEntity<BaseResponseBody> analyzeImg(@RequestPart(value = "file") @ApiParam(value = "이미지 파일") MultipartFile file, @RequestBody AiInterviewRequestDto aiInterviewRequestDto) {
         String imgUrl = awsS3Service.uploadImage(file);
-        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success", emotionService.getResult(imgUrl)));
+        return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success", aiInterviewService.getResult(imgUrl, aiInterviewRequestDto.getInterviewText())));
     }
+
+
 }
