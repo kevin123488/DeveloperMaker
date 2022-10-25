b9ce7019d7d0e8fe47f27267fddf95e465224f4a
박제학 <wpgkr1093@naver.com>
2022-10-25 09:08:39 +0900

    temp

---
+++ front/src/common/selfstudy.js
@@ -0,0 +1,43 @@
+import axiosInstance from "./axios";
+
+
+
+// 스터디 리스트 요청
+export const getStudyListApi = async () => {
+  const res = await axiosInstance.get("/study/list");
+  return res;
+};
+  
+// 해당과목 내용 불러오기
+export const getStudyInfoApi = async (studyId) => {
+  const res = await axiosInstance.get(`/study/${studyId}`);
+  return res;
+};
+
+
+
+// 퀴즈리스트 요청
+export const getQuizListApi = async () => {
+  const res = await axiosInstance.get("/quiz/list");
+  return res;
+};
+
+// 퀴즈 풀기
+export const postQuizSolveApi = async () => {
+  const res = await axiosInstance.post("/quiz");
+  return res;
+};
+
+// 퀴즈 다시 풀기
+export const putQuizSolveApi = async () => {
+  const res = await axiosInstance.put("/quiz");
+  return res;
+};
+
+
+
+// 코테 리스트 조회
+export const getCodingTestListApi = async () => {
+  const res = await axiosInstance.get("/cote");
+  return res;
+};
