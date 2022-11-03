7689c87de7873b16f7cb02a5bee0db3608d0e62e
남한솔 <hansol620915@naver.com>
2022-11-03 17:25:56 +0900

    [BE] Update: Change Cote API
    - 코딩 테스트 컴파일러 API 변경 및 적용.
    - 코테 관련 Dto 수정 및 생성.

--- backend/src/main/java/com/ssafy/developermaker/domain/codingtest/application/CoteService.java
+++ backend/src/main/java/com/ssafy/developermaker/domain/codingtest/application/CoteService.java
@@ -3,19 +3,31 @@ package com.ssafy.developermaker.domain.codingtest.application;
 import com.ssafy.developermaker.domain.codingtest.dto.CoteListRequestDto;
 import com.ssafy.developermaker.domain.codingtest.dto.CoteListResponseDto;
 import com.ssafy.developermaker.domain.codingtest.dto.CoteRequestDto;
+import com.ssafy.developermaker.domain.codingtest.dto.CoteResultDto;
 import com.ssafy.developermaker.domain.codingtest.entity.Cote;
+import com.ssafy.developermaker.domain.codingtest.entity.UserCote;
 import com.ssafy.developermaker.domain.codingtest.repository.CoteRepository;
 import com.ssafy.developermaker.domain.codingtest.repository.UserCoteRepository;
 import com.ssafy.developermaker.domain.user.entity.User;
 import com.ssafy.developermaker.domain.user.exception.UserNotFoundException;
 import com.ssafy.developermaker.domain.user.repository.UserRepository;
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -29,10 +41,11 @@ public class CoteService {
     private final UserRepository userRepository;
     private final UserCoteRepository userCoteRepository;
     private final CoteRepository coteRepository;
-//    @Value("${properties.file.JDoodle-ID}")
-//    private String clientId;
-//    @Value("${properties.file.JDoodle-secret}")
-//    private String clientSecret;
+    @Value("${properties.file.HOST_JAVA}")
+    private String rapidAPI_HOST;
+    @Value("${properties.file.KEY_JAVA}")
+    private String rapidAPI_KEY;
+
 
     public List<CoteListResponseDto> getList(String email, CoteListRequestDto coteListRequestDto) {
         Optional<User> findUser = userRepository.findByEmail(email);
@@ -42,74 +55,96 @@ public class CoteService {
         Page<Cote> page = coteRepository.findAll(pageRequest);
 
         return page.stream().map(cote ->
-                new CoteListResponseDto(cote.getCoteId(), cote.getTitle(), cote.getProblem(),
-                        userCoteRepository.findByUserAndCote(user,cote).isPresent()
-                ? userCoteRepository.findByUserAndCote(user,cote).get().getCorrect() : 0))
+                        new CoteListResponseDto(cote.getCoteId(), cote.getTitle(), cote.getProblem(),
+                                userCoteRepository.findByUserAndCote(user, cote).isPresent()
+                                        ? userCoteRepository.findByUserAndCote(user, cote).get().getCorrect() : 0))
                 .collect(Collectors.toList());
     }
 
     public String submitCote(String email, Long coteId, CoteRequestDto coteRequestDto) {
         String response = "";
-//        Cote cote = coteRepository.findById(coteId).get();
-//        User user = userRepository.findByEmail(email).get();
-//
-//        UserCote userCote;
-//        Optional<UserCote> userCoteOpt = userCoteRepository.findByUserAndCote(user, cote);
-//
-//        // 정답 여부 API 통해 연산.
-//        boolean correct = false;
-//
-//        String script = coteRequestDto.getCode();
-//        String language = coteRequestDto.getLanguage();
-//        String versionIndex = "0";
-//        try {
-//            URL url = new URL("https://api.jdoodle.com/v1/execute");
-//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//            connection.setDoOutput(true);
-//            connection.setRequestMethod("POST");
-//            connection.setRequestProperty("Content-Type", "application/json");
-//
-//            String input = "{\"clientId\": \"" + clientId + "\",\"clientSecret\":\"" + clientSecret + "\",\"script\":\"" + script +
-//                    "\",\"language\":\"" + language + "\",\"versionIndex\":\"" + versionIndex + "\"} ";
-//
-//            System.out.println(input);
-//
-//            OutputStream outputStream = connection.getOutputStream();
-//            outputStream.write(input.getBytes());
-//            outputStream.flush();
-//
-//            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
-//                throw new RuntimeException("Please check your inputs : HTTP error code : " + connection.getResponseCode());
-//            }
-//
-//            BufferedReader bufferedReader;
-//            bufferedReader = new BufferedReader(new InputStreamReader(
-//                    (connection.getInputStream())));
-//
-//            String output;
-//            System.out.println("Output from JDoodle .... \n");
-//            while ((output = bufferedReader.readLine()) != null) {
-//                System.out.println(output);
-//            }
-//
-//            connection.disconnect();
-//
-//        } catch (MalformedURLException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//
-//
-//        if (userCoteOpt.isPresent()) { // 푼 기록이 있을 때.
-//            userCote = userCoteOpt.get();
-//            if (correct) {
-//            }
-//        } else {
-//            userCote = new UserCote();
-//        }
+        Cote cote = coteRepository.findById(coteId).get();
+        User user = userRepository.findByEmail(email).get();
+
+        UserCote userCote;
+        Optional<UserCote> userCoteOpt = userCoteRepository.findByUserAndCote(user, cote);
+
+        // 정답 여부 API 통해 연산.
+        boolean correct = false;
+
+        String code = coteRequestDto.getCode();
+        CoteResultDto coteResultDto;
+        coteResultDto = javaCompilerApi(coteRequestDto);
+
+
+        if (userCoteOpt.isPresent()) { // 푼 기록이 있을 때.
+            userCote = userCoteOpt.get();
+            if (correct) {
+            }
+        } else {
+            userCote = new UserCote();
+        }
 
 
         return response;
     }
+
+    public CoteResultDto javaCompilerApi(CoteRequestDto coteRequestDto) {
+        CoteResultDto coteResultDto = CoteResultDto.builder().build();
+        String language = "0";
+        if (coteRequestDto.getLanguage().equals("java")) {
+            language = "4";
+        } else if (coteRequestDto.getLanguage().equals("python")) {
+            language = "5";
+        } else if (coteRequestDto.getLanguage().equals("cpp")) {
+            language = "7";
+        }
+
+
+        String reqURL = "https://code-compiler.p.rapidapi.com/v2";
+        try {
+            URL url = new URL(reqURL);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setRequestMethod("POST");
+
+            conn.setDoOutput(true);
+            conn.setRequestProperty("content-type", "application/json");
+            conn.setRequestProperty("X-RapidAPI-Key", rapidAPI_KEY);
+            conn.setRequestProperty("X-RapidAPI-Host", rapidAPI_HOST);
+
+
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        JSONObject jsonObject =  new JSONObject();
+        jsonObject.put("LanguageChoice",language);
+        jsonObject.put("Program",coteRequestDto.getCode());
+        jsonObject.put("Input",coteRequestDto.getInput());
+
+
+        HttpResponse<String> response = Unirest.post("https://code-compiler.p.rapidapi.com/v2")
+                .header("content-type", "application/json")
+                .header("X-RapidAPI-Key", rapidAPI_KEY)
+                .header("X-RapidAPI-Host", rapidAPI_HOST)
+                .body(jsonObject.toString())
+                .asString();
+        try {
+            JSONParser parser = new JSONParser();
+            JSONObject jsonObj = (JSONObject) parser.parse(response.getBody());
+            if(jsonObj.get("Errors") != null){
+                coteResultDto.setOutput(jsonObj.get("Errors").toString());
+            } else if (jsonObj.get("Result") != null) {
+                coteResultDto.setOutput(jsonObj.get("Result").toString());
+            }
+        }catch (ParseException e){
+            e.printStackTrace();
+        }
+        System.out.println(response.getBody());
+
+        return CoteResultDto.builder().build();
+    }
+
+
 }
