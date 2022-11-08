191a88cccfc8c892a4318c6f89d3b99574e4b0bd
전현우 <soshot22@naver.com>
2022-11-08 17:41:31 +0900

    Fix: merge conflict -컨플릭트 수정

--- front/src/components/SelfStudy/LangFrameSelfStudy.js
+++ front/src/components/SelfStudy/LangFrameSelfStudy.js
@@ -44,21 +44,21 @@ const LangFrameStudy = () => {
   }
   const webcamRef = React.useRef(null);
 
-  // function dataURItoBlob(dataURI) {
+  function dataURItoBlob(dataURI) {
 
-  //   var byteString = atob(dataURI.split(',')[1]);
-  //   var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+    var byteString = atob(dataURI.split(',')[1]);
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
     
-  //   var ab = new ArrayBuffer(byteString.length);
-  //   var ia = new Uint8Array(ab);
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
     
-  //   for (var i = 0; i < byteString.length; i++) {
-  //     ia[i] = byteString.charCodeAt(i);
-  //   }
-  //   var blob = new Blob([ab], {type: mimeString});
+    for (var i = 0; i < byteString.length; i++) {
+      ia[i] = byteString.charCodeAt(i);
+    }
+    var blob = new Blob([ab], {type: mimeString});
     
-  //   return blob;
-  // }
+    return blob;
+  }
 
   // 캡쳐하기
   const [imageSrc, setImageSrc] = useState('')
