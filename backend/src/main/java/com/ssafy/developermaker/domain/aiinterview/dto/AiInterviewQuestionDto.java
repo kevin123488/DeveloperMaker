71f7ee543e02f75debe5521f6d9cc4a3716da57c
남한솔 <hansol620915@naver.com>
2022-11-14 09:18:45 +0900

    [BE] Fix: AIDto
    - AI Dto Keyword 리스트로 변경

--- backend/src/main/java/com/ssafy/developermaker/domain/aiinterview/dto/AiInterviewQuestionDto.java
+++ backend/src/main/java/com/ssafy/developermaker/domain/aiinterview/dto/AiInterviewQuestionDto.java
@@ -2,13 +2,17 @@ package com.ssafy.developermaker.domain.aiinterview.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
 
 @Builder
 @Getter
+@Setter
 public class AiInterviewQuestionDto {
     private Long aiqId;
     private Integer no;
     private String subject;
     private String question;
-    private String keyword;
+    private List<String> keyword;
 }
